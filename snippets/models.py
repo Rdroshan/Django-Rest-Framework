b04fc951e3fbef5bf9142a1f249b41e889642b40
roshan dawande <roshan.dawande@vogo.in>
2019-09-22 12:51:46 +0530

    Adding information to our model

--- snippets/models.py
+++ snippets/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -17,7 +20,27 @@ class Snippet(models.Model):
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
     highlighted = models.TextField()
+    """
+    related_name or reverse relationship here is snippets.
+    What it means is that we can use access the snippets associated with a owner.
+    for more understanding, refer this: https://stackoverflow.com/questions/17328910/django-what-is-reverse-relationship
+    """
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name="snippets")
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ['created']
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                  full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
 
